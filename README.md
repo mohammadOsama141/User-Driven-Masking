24ef474f147cc4b6d97b549a475b44da1acc687e
mohammadOsama141 <mhd.osama141@gmail.com>
2023-12-11 00:25:24 -0800

    On branch main  Initial commit Changes to be committed: 	new file:   README.md 	new file:   main.py 	new file:   static/23.png 	new file:   static/mask.png 	new file:   static/result.png 	new file:   templates/index.html

---
+++ README.md
@@ -0,0 +1,10 @@
+Interactive Brush Masking
+About the Project
+Interactive Brush Masking is a web-based application that allows users to interactively create masks on images using a brush-like tool. This project is built using Flask, a Python web framework, and incorporates HTML, JavaScript, and OpenCV for image processing. Users can click and drag their mouse over the image to draw a mask, which is updated in real-time.
+
+Features
+Real-Time Interaction: Users can draw on an image to create a mask, seeing the results of their actions immediately.
+User-Driven Masking: The masking process is entirely controlled by the user, offering a high degree of precision.
+Image and Mask Output: The application outputs both the original image with the mask applied and a separate mask image.
+How It Works
+The application uses Flask to serve a webpage where an image is displayed. JavaScript is used to capture mouse events when a user clicks and drags on the image. These points are sent to the Flask server, which uses OpenCV to create and update a mask based on the user's input. The mask is then applied to the original image, and both the masked image and the mask itself are saved and updated on the webpage.
---
+++ main.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify, render_template
+import numpy as np
+import cv2
+from PIL import Image
+
+app = Flask(__name__)
+
+# Load and prepare the image
+raw_image = Image.open('static/23.png').convert("RGB")
+image_height, image_width = raw_image.size
+mask = np.zeros((image_width, image_height), dtype=np.uint8)  # Initialize mask
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/draw_mask', methods=['POST'])
+def draw_mask():
+    data = request.json
+    point = data['point']
+    brush_size = 10  # Adjust brush size as needed
+
+    # Update mask
+    cv2.circle(mask, (int(point['x']), int(point['y'])), brush_size, 1, -1)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (brush_size, brush_size))
+    mask_dilated = cv2.dilate(mask, kernel)
+
+    # Apply mask to image
+    orig_img = np.array(raw_image)
+    mask_img = np.stack([mask_dilated * 255] * 3, axis=-1)
+    final_img = cv2.addWeighted(orig_img, 1, mask_img, 0.5, 0)
+    final_img = cv2.cvtColor(final_img, cv2.COLOR_BGR2RGB)
+
+    # Save the combined image
+    result_path = 'static/result.png'
+    cv2.imwrite(result_path, final_img)
+
+    # Save the mask image
+    mask_path = 'static/mask.png'
+    cv2.imwrite(mask_path, mask_dilated * 255)  # Save mask as a black & white image
+
+    return jsonify({'image_path': result_path, 'mask_path': mask_path})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
